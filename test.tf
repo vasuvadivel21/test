e690d0b7e6e44b200d034b85a93f0fa5874e610a
vasu <vasuvadivel21@gmail.com>
2022-05-03 22:08:29 +0530

    tf file

---
+++ test.tf
@@ -0,0 +1,12 @@
+variable "ami" {
+    description = "instance ami"
+    default ="ami-0f9fc25dd2506cf6d"
+}
+variable "ins-type" {
+    default = "t2.micro"
+}
+
+variable "my-template" {
+    default = "lt-0b42c17646182ca4f"
+  
+}
